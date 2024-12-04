3b0d530f9a9b07b26c06f902d2b164123209da63
NguyenDinhNhan1510 <dinhnhan151004@gmail.com>
2024-12-04 09:55:02 +0700

    Add project files.

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,40 @@
+﻿using Fruit_N12.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Fruit_N12.Controllers
+{
+    public class ContactController : Controller
+    {
+
+        private readonly FruitN12Context _context;
+
+        public ContactController(FruitN12Context context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string name, string email, string message)
+        {
+            try
+            {
+                TbContact contact = new TbContact();
+                contact.Name = name;
+                contact.Email = email;
+                contact.Message = message;
+                _context.Add(contact);
+                _context.SaveChangesAsync();
+                return Json(new { status = true });
+            }
+            catch
+            {
+                return Json(new { status = false });
+            }
+        }
+    }
+}
